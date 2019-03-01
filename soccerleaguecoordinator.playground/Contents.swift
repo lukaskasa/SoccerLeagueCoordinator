daa4a38b6088269f35a6f048c3d8818fbf31872b
Lukas Kasakaitis <lukas.kasakaitis@icloud.com>
2019-03-01 08:19:46 +0100

    Refactor player collections.

--- soccerleaguecoordinator.playground/Contents.swift
+++ soccerleaguecoordinator.playground/Contents.swift
@@ -6,36 +6,34 @@
 */
 
 // Dictionary to store Soccer League Players
-let players : [String : [String : Any]] = [
-    "Joe Smith" : ["Height": 42.0, "Experience": true, "Guardians": "Jim and Jan Smith"],
-    "Jill Tanner" : ["Height": 36.0, "Experience": true, "Guardians": "Clara Tanner"],
-    "Bill Bon" : ["Height": 43.0, "Experience": true, "Guardians": "Sara and Jenny Bon"],
-    "Eva Gordon" : ["Height": 45.0, "Experience": false, "Guardians": "Wendy and Mike Gordon"],
-    "Matt Gill" : ["Height": 40.0, "Experience": false, "Guardians": "Charles and Sylvia Gill"],
-    "Kimmy Stein" : ["Height": 41.0, "Experience": false, "Guardians": "Bill and Hillary Stein"],
-    "Sammy Adams" : ["Height": 45.0, "Experience": false, "Guardians": "Jeff Adams"],
-    "Karl Saygan" : ["Height": 42.0, "Experience": true, "Guardians": "Heather Bledsoe"],
-    "Suzane Greenberg" : ["Height": 44.0, "Experience": true, "Guardians": "Henrietta Dumas"],
-    "Sal Dali" : ["Height": 41.0, "Experience": false, "Guardians": "Gala Dali"],
-    "Joe Kavalier" : ["Height": 39.0, "Experience": false, "Guardians": "Sam and Elaine Kavalier"],
-    "Ben Finkelstein" : ["Height": 44.0, "Experience": false, "Guardians": "Aaron and Jill Finkelstein"],
-    "Diego Soto" : ["Height": 41.0, "Experience": true, "Guardians": "Robin and Sarika Soto"],
-    "Chloe Alaska" : ["Height": 47.0, "Experience": false, "Guardians": "David and Jamie Alaska"],
-    "Arnold Willis" : ["Height": 43.0, "Experience": false, "Guardians": "Claire Willis"],
-    "Phillip Helm" : ["Height": 44.0, "Experience": true, "Guardians": "Thomas Helm and Eva Jones"],
-    "Les Clay" : ["Height": 42.0, "Experience": true, "Guardians": "Wynonna Brown"],
-    "Herschel Krustofski" : ["Height": 45.0, "Experience": true, "Guardians": "Hyman and Rachel Krustofski"]
-]
+
+let joeSmith: [String: Any] = ["name": "Joe Smith", "height": 42.0, "isExperienced": true, "guardians": "Jim and Jan Smith"]
+let jillTanner: [String: Any] = ["name": "Jill Tanner", "height": 36.0, "isExperienced": true, "guardians": "Clara Tanner"]
+let billBon: [String: Any] = ["name": "Bill Bon", "height": 43.0, "isExperienced": true, "guardians": "Sara and Jenny Bon"]
+let evaGordon: [String: Any] = ["name": "Eva Gordon", "height": 45.0, "isExperienced": false, "guardians": "Wendy and Mike Gordon"]
+let mattGill: [String: Any] = ["name": "Matt Gill", "height": 40.0, "isExperienced": false, "guardians": "Charles and Sylvia Gill"]
+let kimmyStein: [String: Any] = ["name": "Kimmy Stein", "height": 41.0, "isExperienced": false, "guardians": "Bill and Hillary Stein"]
+let sammyAdams: [String: Any] = ["name": "Sammy Adams", "height": 45.0, "isExperienced": false, "guardians": "Jeff Adams"]
+let karlSaygan: [String: Any] =  ["name": "Karl Saygan", "height": 42.0, "isExperienced": true, "guardians": "Heather Bledsoe"]
+let suzanneGreenberg: [String: Any] = ["name": "Suzane Greenberg", "height": 44.0, "isExperienced": true, "guardians": "Henrietta Dumas"]
+let salDali: [String: Any] = ["name": "Sal Dali", "height": 41.0, "isExperienced": false, "guardians": "Gala Dali"]
+let joeKavalier: [String: Any] = ["name": "Joe Kavalier", "height": 39.0, "isExperienced": false, "guardians": "Sam and Elaine Kavalier"]
+let benFinkelstein: [String: Any] = ["name": "Ben Finkelstein", "height": 44.0, "isExperienced": false, "guardians": "Aaron and Jill Finkelstein"]
+let diegoSoto: [String: Any] = ["name": "Diego Soto", "height": 41.0, "isExperienced": true, "guardians": "Robin and Sarika Soto"]
+let chloeAlaska: [String: Any] = ["name": "Chloe Alaska", "height": 47.0, "isExperienced": false, "guardians": "David and Jamie Alaska"]
+let arnoldWillis: [String: Any] = ["name": "Arnold Willis", "height": 43.0, "isExperienced": false, "guardians": "Claire Willis"]
+let phillipHelm: [String: Any] = ["name": "Phillip Helm", "height": 43.0, "isExperienced": true, "guardians": "Claire Willis"]
+let lesClay: [String: Any] = ["name": "Les Clay", "height": 42.0, "isExperienced": true, "guardians": "Wynonna Brown"]
+let herschelKrustofski: [String: Any] = ["name": "Herschel Krustofski", "height": 45.0, "isExperienced": true, "guardians": "Hyman and Rachel Krustofski"]
+
+let players = [joeSmith, jillTanner, billBon, evaGordon, mattGill, kimmyStein, sammyAdams, karlSaygan, suzanneGreenberg, salDali, joeKavalier, benFinkelstein, diegoSoto, chloeAlaska, arnoldWillis, phillipHelm, lesClay, herschelKrustofski]
 
 // Function to Calculate Number of Experienced Players
-func numberOfExperienced(_ players: [String: [String: Any]]) -> Int {
+func numberOfExperienced(_ players: [[String: Any]]) -> Int {
     var numberOfExperiencedPlayers = 0
     
     for player in players {
-        
-        let experience = player.value["Experience"]! as! Bool
-        
-        if experience {
+        if isExperienced(player) {
             numberOfExperiencedPlayers += 1
         }
     }
@@ -46,7 +44,7 @@ func numberOfExperienced(_ players: [String: [String: Any]]) -> Int {
 // Function to Find out if player is experienced
 
 func isExperienced(_ player: [String : Any]) -> Bool {
-    let withExperience = player["Experience"]! as! Bool
+    let withExperience = player["isExperienced"]! as! Bool
     
     if withExperience {
         return true
@@ -57,11 +55,10 @@ func isExperienced(_ player: [String : Any]) -> Bool {
 
 // Function to Calculate AverageHeight
 
-func averageHeightOf(_ team: [String : [String : Any]]) -> Double {
+func averageHeightOf(_ team: [[String : Any]]) -> Double {
     var averageHeight : Double = 0.0
-    
     for player in team {
-        averageHeight += player.value["Height"] as! Double
+        averageHeight += player["height"] as! Double
     }
     
     switch team.count {
@@ -76,7 +73,7 @@ func sumOfHeights(_ team: [String : [String : Any]]) -> Double {
     var sum : Double = 0.0
     
     for player in team {
-        sum += player.value["Height"] as! Double
+        sum += player.value["height"] as! Double
     }
     
     return sum
@@ -84,19 +81,19 @@ func sumOfHeights(_ team: [String : [String : Any]]) -> Double {
 
 // Function to seperate Experienced from inexperienced
 
-func filterPlayers(experienced: Bool, _ players: [String : [String : Any]]) -> [String : [String : Any]] {
+func filterPlayers(experienced: Bool, _ players: [[String : Any]]) -> [[String : Any]] {
     var players = players
-    var sortedPlayers = [String : [String : Any]]()
+    var sortedPlayers = [[String : Any]]()
     
     for player in players {
-        if experienced && isExperienced(player.value) {
-            sortedPlayers.updateValue(player.value, forKey: player.key)
-            players.removeValue(forKey:  player.key)
-        } else if !experienced && !isExperienced(player.value){
-            sortedPlayers.updateValue(player.value, forKey: player.key)
-            players.removeValue(forKey: player.key)
+        if experienced && isExperienced(player) {
+            sortedPlayers.append(player)
+             players.removeAll(where: {$0["name"] as! String == player["name"] as! String})
+        } else if !experienced && !isExperienced(player){
+            sortedPlayers.append(player)
+            players.removeAll(where: {$0["name"] as! String == player["name"] as! String})
         } else {
-            players.removeValue(forKey: player.key)
+             players.removeAll(where: {$0["name"] as! String == player["name"] as! String})
         }
     }
     
@@ -105,67 +102,73 @@ func filterPlayers(experienced: Bool, _ players: [String : [String : Any]]) -> [
 
 // Function to devide players into teams
 
-func divideInto(teamNames: [String], with players: [String : [String : Any]]) -> [String : [String: [String: Any]]] {
+func divideInto(teamNames: [String], with players: [[String : Any]]) -> [String : [[String: Any]]] {
     let playersPerTeam = players.count / teamNames.count
     let experiencedPlayersPerTeam = numberOfExperienced(players) / teamNames.count
     
-    var playerSets = [filterPlayers(experienced: true, players), filterPlayers(experienced: false, players)]
-    var teams = [String : [String: [String: Any]]]()
-    var tallestPlayer = [String: [String: Any]]()
+    var playerSets = [[String : Any]]()
+    playerSets.append(contentsOf: filterPlayers(experienced: true, players))
+    playerSets.append(contentsOf: filterPlayers(experienced: false, players))
+    var teams = [String: [[String: Any]] ]()
+    var tallestPlayer = [[String: Any]]()
     var tallestPlayerHeight = Double()
     
     // Create the number of dictionaries as there are teams
     for i in 0..<teamNames.count {
-        teams[teamNames[i]] = [:]
+        teams[teamNames[i]] = []
     }
     
     var index : Int = 0
     
     // Continue to loop until dictionaries are empty
-    while playerSets[0].count > 0 && playerSets[1].count > 0 {
+    while playerSets.count > 0 {
         
-        for set in playerSets {
-            for player in set {
+            for player in playerSets {
                 // Retrieve the tallest player first
-                let playerHeight =  player.value["Height"] as! Double
+                let playerHeight =  player["height"] as! Double
                 
                 if tallestPlayer.count < 1 {
-                    tallestPlayer[player.key] = player.value
-                    tallestPlayerHeight = tallestPlayer.first?.value["Height"] as! Double
+                    tallestPlayer.append(player)
+                    tallestPlayerHeight = tallestPlayer[0]["height"] as! Double
                 } else if playerHeight > tallestPlayerHeight {
                     tallestPlayer.removeAll()
-                    tallestPlayer[player.key] = player.value
-                    tallestPlayerHeight = tallestPlayer.first?.value["Height"] as! Double
+                    tallestPlayer.append(player)
+                    tallestPlayerHeight = tallestPlayer[0]["height"] as! Double
                 }
             }
+        
+            guard let name = tallestPlayer[0]["name"] else { fatalError("Name doesn't Exist") }
             
             // Tallest players get into the team first
-            if isExperienced(tallestPlayer.first!.value) && numberOfExperienced(teams[teamNames[index]]!) != experiencedPlayersPerTeam {
-                teams[teamNames[index]]!.updateValue(tallestPlayer.first!.value, forKey: tallestPlayer.first!.key)
-                playerSets[0].removeValue(forKey: tallestPlayer.first!.key)
+            if isExperienced(tallestPlayer[0]) && numberOfExperienced(teams[teamNames[index]]!) != experiencedPlayersPerTeam {
+                
+                teams[teamNames[index]]!.append(tallestPlayer[0])
+                playerSets.removeAll(where: {$0["name"] as! String == name as! String})
                 tallestPlayer.removeAll()
+                
             } else if teams[teamNames[index]]!.count != playersPerTeam {
-                teams[teamNames[index]]!.updateValue(tallestPlayer.first!.value, forKey: tallestPlayer.first!.key)
-                playerSets[1].removeValue(forKey: tallestPlayer.first!.key)
+                
+                teams[teamNames[index]]!.append(tallestPlayer[0])
+                playerSets.removeAll(where: {$0["name"] as! String == name as! String})
                 tallestPlayer.removeAll()
+                
             }
-        }
+        
+            // Increase Index in order to distrubute players evenly
+            if index == teamNames.count - 1 {
+                index = 0
+            } else {
+                index += 1
+            }
        
-        // Increase Index in order to distrubute players evenly
-        if index == teamNames.count - 1 {
-            index = 0
-        } else {
-            index += 1
-        }
-
     }
-
+    
     return teams
 }
 
 
 // Function to create letters which returns an Array of Strings
-func writeLetterTo(_ teams: [String : [String : [String : Any]]]) -> [String] {
+func writeLetterTo(_ teams: [String : [[String : Any]]]) -> [String] {
     var letters = [String]()
     var letter : String = ""
     var practiceTime : String = ""
@@ -182,11 +185,11 @@ func writeLetterTo(_ teams: [String : [String : [String : Any]]]) -> [String] {
         }
         
         for player in team.value {
-            let guardians = player.value["Guardians"]!
-            let playerName = player.key
-            
+            let guardians = player["guardians"]!
+            let playerName = player["name"]!
+
             letter = "Dear \(guardians), \n your child \(playerName) has been selected by team \(teamName). Practice starts on \(practiceTime). We are excited to meet \(playerName) \n Best Regards \n Team \(teamName)"
-            
+
             letters.append(letter)
         }
     }
@@ -206,7 +209,7 @@ func printLetters(letters: [String]) -> Void {
 
 
 //Print All teeam
-
+ divideInto(teamNames: ["Sharks", "Dragons", "Raptors"], with: players)
 var teams = divideInto(teamNames: ["Sharks", "Dragons", "Raptors"], with: players)
 
 var teamSharks = teams["Sharks"]
@@ -239,6 +242,13 @@ let letters = writeLetterTo(teams)
 printLetters(letters: letters)
 
 
-
+/**
+ @lukaskasa A couple of things:
+ - Your player dictionaries should look a bit different:
+ `let joeSmith: [String: Any] = ["name": "Joe Smith", "height": 42.0, "isExperienced": true, "guardians": "Jim and Jan Smith"]` for example. Notice that the key names are lowercased to conform to Apple’s API Design Guidelines. Also, getting the name of the player is now easier since it has its own key.
+ - `let experience = player.value["Experience"]! as! Bool` force unwrapping optionals isn’t the best idea. Although not covered in unit 1 here’s a link with an introduction to optionals in Swift.
+ 
+   https://parussini.codes/2019/02/13/the-techdegree-blog-optionals-in-swift-and-how-to-deal-with-them/
+ **/
 
 
